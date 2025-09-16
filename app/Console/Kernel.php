e625c8338e8165a8fc1387d671962a9bd311ada2
tranvid25 <lsdnh041004@gmail.com>
2025-09-16 07:08:03 +0700

    feat/register

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\MakeServiceCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -23,5 +24,11 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
+
     }
+    protected $commands = [
+    \App\Console\Commands\MakeServiceCommand::class,
+    
+];
+
 }
---
+++ app/Repositories/UserRepositories.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Repositories;
+use App\Models\User;
+class UserRepositories{
+   public function create(array $data):User{
+     return User::create($data);
+   }
+   public function findByEmail(string $email):?User{
+    return User::where('email',$email)->first();
+   }
+}
