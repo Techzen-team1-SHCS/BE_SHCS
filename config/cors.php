5d0fec05f9c425dd923effbadcbd1c6686f68d0c
TranVi <vit76404@gmail.com>
2026-03-14 15:06:39 +0700

    fix

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        'http://localhost:5173',
+        env('FRONTEND_URL'),
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
