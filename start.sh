723bb762d7495d6eacddcb1eedbc701363370b66
TranVi <vit76404@gmail.com>
2026-03-16 23:20:55 +0700

    Deploy reverb

--- start.sh
+++ start.sh
@@ -10,5 +10,5 @@ php artisan route:cache
 echo "Start Reverb"
 php artisan reverb:start --host=0.0.0.0 --port=8080 &
 
-echo "Start Laravel"
-php artisan serve --host=0.0.0.0 --port=$PORT
+echo "Starting nginx..."
+/start.sh
